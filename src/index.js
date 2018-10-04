d075eda614b73a92ea73279df9a489f8142dadcc
Henrique Souza <henriquesouza@Henriques-MacBook-Pro.local>
2018-10-04 00:53:23 -0300

    Video list and Video search

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
-const API_KEY = 'AIzaSyClKANnTXTsBv8pUE3go0haIVudF8JXZTM';
+const API_KEY = "AIzaSyClKANnTXTsBv8pUE3go0haIVudF8JXZTM";
 
-// Create a new component.
-//This component should be produce html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 // Take this component's generated html and
 //put it on the page (in the DOM)
